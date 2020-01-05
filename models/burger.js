5aeb468bb3eec9244874ccab950f29c55d5f476b
uchrissd <uchrissd@protonmail.com>
2020-01-05 13:42:09 -0800

    corrected syntax error in model

--- models/burger.js
+++ models/burger.js
@@ -7,12 +7,12 @@ var burger = {
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+    orm.instertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
