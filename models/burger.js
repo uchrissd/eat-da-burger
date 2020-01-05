64adb266f1af9ed2f4e51c7c1c4edc07e5a3a0de
uchrissd <uchrissd@protonmail.com>
2020-01-04 16:38:07 -0800

    make it better

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,22 @@
 var orm = require("../config/orm");
 
-var burger = "";
+var burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
 // Export the database functions for the controller (catsController.js).
 module.exports = burger;
