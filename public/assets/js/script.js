1549f802d6e9fe798ee127f06a667a36ab3240af
uchrissd <uchrissd@protonmail.com>
2020-01-04 17:10:34 -0800

    created the handlebar html files and the script file

---
+++ public/assets/js/script.js
@@ -0,0 +1,56 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour");
+
+    var newDevourState = {
+      devour: newDevour
+    };
+
+    // Send the PUT request.
+    $.ajax("/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(function() {
+      console.log("devour changed to", newDevour);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#burgerName")
+        .val()
+        .trim(),
+      devoured: true
+    };
+
+    // Send the POST request.
+    $.ajax("/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("new burger added");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/burgers/" + id, {
+      type: "DELETE"
+    }).then(function() {
+      console.log("burger deleted", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
